7151ba443e388552ed564bb49c4fc67242231b1e
Mohamed <s2000588@edu.bc.fi>
2023-05-30 19:46:06 +0300

    create cartscreen

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,133 @@
+import React from "react";
+import { Link, useParams, useLocation, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { FaTrash } from "react-icons/fa";
+
+import Message from "../components/Message";
+import { addToCart } from "../slices/cartSlice";
+
+const CartScreen = () => {
+  const { id: productId } = useParams();
+
+  const { cartItems } = useSelector((state) => state.cart);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const getCartCount = () => {
+    // reduce takes the accumulator and the current item
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  };
+
+  const getCartSubTotal = () => {
+    return cartItems
+      .reduce((price, item) => price + item.qty * item.price, 0)
+      .toFixed(2);
+  };
+
+  // this will help to change the qty of the product in the cart
+  const addToCartHandler = async (product, qty) => {
+    dispatch(addToCart({ ...product, qty }));
+  }
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: "20px" }}>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>{" "}
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) => addToCartHandler(item, Number(e.target.value))}  >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type='button'
+                      variant='light'
+                      onClick={() =>
+                        dispatch({
+                          type: "CART_REMOVE_ITEM",
+                          payload: item._id,
+                        })
+                      }
+                    >
+                      <FaTrash />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              {/* <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2> */}
+              <h2>SubTitle ({getCartCount()}) Items</h2>€{getCartSubTotal()}
+            </ListGroup.Item>
+            {/* <ListGroup.Item>
+              <Row>
+                <Col>Items</Col>
+                <Col>
+                  $
+                  {cartItems
+                    .reduce((acc, item) => acc + item.qty * item.price, 0)
+                    .toFixed(2)}
+                </Col>
+              </Row>
+            </ListGroup.Item> */}
+            <ListGroup.Item>
+              <Button
+                type='button'
+                className='btn-block'
+                disabled={cartItems.length === 0}
+                onClick={() => navigate("/login?redirect=shipping")}
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+
+export default CartScreen;

1bb94d925ee6dd24b66316a30234dcb11f8dfcaf
Mohamed <s2000588@edu.bc.fi>
2023-05-30 09:57:43 +0300

    create reating component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,44 +1,43 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, Container, NavDropdown } from "react-bootstrap";
 import { FaShoppingCart, FaUber } from "react-icons/fa";
+
 import Logo from "../assets/logo.jpg";
+
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          {/* <LinkContainer to='/'>
-            <Navbar.Brand>Q-shop</Navbar.Brand>
-          </LinkContainer> */}
-          <Navbar.Brand href='/'>
-            <img
-              src={Logo}
-              alt='logo'
-              width='30'
-              height='30'
-              className='d-inline-block  mx-2 my-2 rounded-circle '
-            />
-            shop
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img
+                src={Logo}
+                alt='logo'
+                width='30'
+                height='30'
+                className='d-inline-block  mx-2 my-2 rounded-circle '
+              />
+              shop
+            </Navbar.Brand>
+          </LinkContainer>
 
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
-                <FaShoppingCart />
-                Cart
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <FaUber />
-                Sign In
-              </Nav.Link>
-
-              {/* <LinkContainer to='/cart'>
+              <LinkContainer to='/cart'>
                 <Nav.Link>
-                  <i className='fas fa-shopping-cart mx-1'></i>
+                  <FaShoppingCart />
                   Cart
                 </Nav.Link>
-              </LinkContainer> */}
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <FaUber />
+                  Sign In
+                </Nav.Link>
+              </LinkContainer>
 
               {/* {userInfo ? (
                 <NavDropdown title={userInfo.name} id='username'>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-import './assets/styles/index.css';
-import './assets/styles/bootstrap.custom.css';
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>
 );
 

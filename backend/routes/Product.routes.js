18b29fdedfed12e1eb9b6c0a1084245d27c9cba9
Mohamed <s2000588@edu.bc.fi>
2023-06-01 18:58:01 +0300

    add delete product route

--- backend/routes/Product.routes.js
+++ backend/routes/Product.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import {
   createProduct,
+  deleteProduct,
   getProductById,
   getProducts,
   updateProduct,
@@ -12,6 +13,10 @@ import { protect, admin } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;

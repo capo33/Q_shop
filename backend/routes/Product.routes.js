6d7e881e20d36d19dc363cd93e04ae87f9df9b82
Mohamed <s2000588@edu.bc.fi>
2023-06-02 08:19:17 +0300

    create product review router

--- backend/routes/Product.routes.js
+++ backend/routes/Product.routes.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import {
   createProduct,
+  createProductReview,
   deleteProduct,
   getProductById,
   getProducts,
@@ -18,5 +19,6 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 export default router;
